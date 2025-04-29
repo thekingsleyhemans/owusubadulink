0445e25a51eeda45f5830aef3af39e5c7a298c8b
hemansjr <46025171+thekingsleyhemans@users.noreply.github.com>
2025-04-29 16:34:40 +0000

    js added

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ linkItems.forEach((item) => {
     const linkText = item.querySelector('.link-text');
 
     // Expand the width of the current link-item
-    //item.style.transition = 'width 0.3s ease-in-ou'; // Smooth transition
-    //item.style.width = '150%'; // Adjust the width as needed
+    item.style.transition = 'width 0.3s ease-in-ou'; // Smooth transition
+    item.style.width = '150%'; // Adjust the width as needed
 
     // Remove the 'hidden' class from the marquee content
     if (marqueeContent) {
